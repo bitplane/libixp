60e0eb5ad3051c2bb83e3b3498812dd392bd20c8
Gareth Davidson <gaz@bitplane.net>
2026-09-09 01:33:30 +0100

    lib/libixp: avoid retaining freed pending links

--- lib/libixp/srv_util.c
+++ lib/libixp/srv_util.c
@@ -363,7 +363,7 @@ ixp_pending_write(IxpPending *pending, const char *dat, long ndat) {
 	IxpRequestLink req_link;
 	IxpQueue **qp, *queue;
 	IxpPendingLink *pp;
-	IxpRequestLink *rp;
+	Ixp9Req *req;
 
 	if(ndat == 0)
 		return;
@@ -396,8 +396,10 @@ ixp_pending_write(IxpPending *pending, const char *dat, long ndat) {
 	req_link.prev->next = &req_link;
 	req_link.next->prev = &req_link;
 
-	while((rp = req_link.next) != &req_link)
-		ixp_pending_respond(rp->req);
+	while(req_link.next != &req_link) {
+		req = req_link.next->req;
+		ixp_pending_respond(req);
+	}
 }
 
 int
